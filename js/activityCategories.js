7e01403cbea64213469407a32f3c3d3704df530c
Aeternia-ua <gherasymuk@gmail.com>
2020-02-06 02:40:21 +0200

    Update package.json

--- js/activityCategories.js
+++ js/activityCategories.js
@@ -35,40 +35,10 @@ function getCodes(acCodes) {
 };
 
 function mergeCodes(arr, codeArr) {
-	arr.features.forEach(element => {
-		match = codeArr.find(codeElement => codeElement.ac1 == element.properties.ac1);
-		if (match) {
-			console.log("Match found ")
-			// Merge the second array into the first one
-			for (var j = 0; j < codesProperties.length; j++) {
-        		element.properties[codesProperties[j].name] = match[codesProperties[j].name];
-        	}
-		}
-	});
-}
-
-function createMarkerClusterSubgroups(parentMarkerCluster){
- markerClusterSubGroup = L.featureGroup.subGroup(parentMarkerCluster, arrayOfMarkers);
- markerClusterSubGroup.addTo(map);
-}
-
-
-///Doesn't work
-// function createLayers(map, codesJson){
-// 	var categoryLayers = [];
-// 	for (var i = 0; i < codesJson.length; i++) {
-// 		categoryLayers[i] = new L.layerGroup().addTo(map);
-// 	}
-
-// 	return addOverlays(categoryLayers);
-// }
-
-function addOverlays(categoryLayers){
-	var overlays = {}; ///collection feature: value
-	for (var i = 0; i < categoryLayers.length; i++) {
-		// Create map overlays
-		overlays[categoryLayers[i].activitycodename] = categoryLayers[i].activitycodename;
-	}
-	return overlays;
+	codeArr.forEach(code => 
+		arr.features
+		.filter(element => element.properties.ac1 == code.ac1)
+		.forEach(element => codesProperties
+				.forEach(codeProperty => element.properties[codeProperty.name] = code[codeProperty.name])));
 }
 
